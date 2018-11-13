49b3cf62a23334d37093d864820bcd33858f8b94
Gábor Kovács <gabor950601@gmail.com>
2018-11-13 09:49:21 +0100

    task implemented

--- src/main/java/com/codecool/singletonDojo/Printer.java
+++ src/main/java/com/codecool/singletonDojo/Printer.java
@@ -1,20 +1,44 @@
 package com.codecool.singletonDojo;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Printer {
+
+    private static Random rand = new Random();
+
+    private static int printerId;
+    private static Printer instance = null;
+    private static List<Printer> printers = new ArrayList<>();
 
     private int id; // ID of the printer. Unique.
     private LocalTime busyEndTime;
 
-    public Printer(int id) {
+    private Printer(int id) {
         this.id = id;
+        printers.add(this);
+    }
+
+    public static Printer getInstance(){
+        if (printers.size() < 10){
+            instance = new Printer(++printerId);
+        } else {
+            for (Printer printer : printers){
+                if (printer.isAvailable()){
+                    return printer;
+                }
+            }
+            return printers.get(rand.nextInt(printers.size()));
+        }
+        return instance;
     }
 
     // Prints out the given String
     public void print(String toPrint) {
         // Its not needed to actually print with a printer in this exercise
-        System.out.println("Printer " + id + " is printing.");
+        System.out.println("Printer " + id + " is printing this: " + toPrint);
         busyEndTime = LocalTime.now().plusSeconds(5);
     }
 
